fa85fd0504f932dbb9e68031ea295e5be97e73a3
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-07 07:32:25 +0530

    Add New Contact

--- src/main/java/com/bdlz/AB/AddressBook.java
+++ src/main/java/com/bdlz/AB/AddressBook.java
@@ -1,41 +1,63 @@
 package com.bdlz.AB;
 
-import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AddressBook {
-    static class PersonDetails {
-        private String firstName;
-        private String lastName;
-        private String area;
-        private String city;
-        private String state;
-        private int zipCode;
-        private long mobileNo;
-        private String emailId;
+    static class ContactDetails {
+        private List<PersonDetails> contacts;
+
+        public ContactDetails(List<PersonDetails> contacts) {
+            this.contacts=contacts;
+        }
+        public List<PersonDetails> getContacts(){
+            return contacts;
+        }
+        public void setContacts(List<PersonDetails> contacts) {
+            this.contacts = contacts;
+        }
     }
+    public static void contactDisplay(ContactDetails contactDetails) {
+        List<PersonDetails> contactDetailsList = contactDetails.getContacts();
+        for (int i = 0; i < contactDetailsList.size(); i++){
+            PersonDetails person1 = contactDetailsList.get(i);
+            System.out.println("Person Details : ");
+            System.out.println("Name : " + person1.getFirstName() + " " + person1.getLastName());
+            System.out.println("Mobile No : " + person1.getMobileNo());
+            System.out.println("EmailId : " + person1.getEmailId());
+            System.out.println("Address");
+            System.out.println("Area : " + person1.getArea());
+            System.out.println("City : " + person1.getCity());
+            System.out.println("State : " + person1.getState());
+            System.out.println("ZipCode : " + person1.getZipCode());
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Program");
-
-        PersonDetails data = new PersonDetails();
-
-        data.firstName = ("Ramakrishna");
-        data.lastName = ("Velisetti");
-        data.area = ("Sitaramapuram-A");
-        data.city = ("Vijayawada");
-        data.state = ("Andhra Pradesh");
-        data.zipCode = (521109);
-        data.mobileNo = (8500029054L);
-        data.emailId = ("ramakrishna31.velisetti@gmail.com");
-
-        System.out.println("Personal Details:- ");
-        System.out.println("Name" + " : " + data.firstName +" " + data.lastName);
-        System.out.println("Email.Id" + " : " + data.emailId);
-        System.out.println("MobileNo" + " : " + data.mobileNo);
-
-        System.out.println("Address:- ");
-        System.out.println("Area" + " : " + data.area);
-        System.out.println("City" + " : " + data.city);
-        System.out.println("State" + " : " + data.state);
-        System.out.println("ZipCode" + " : " + data.zipCode);
+        Scanner newData = new Scanner(System.in);
+        System.out.println("Enter Details in AddressBook");
+        System.out.println("Enter Your FirstName");
+        String firstName = newData.next();
+        System.out.println("Enter your LastName");
+        String lastName = newData.next();
+        System.out.println("Enter Your Area");
+        String area = newData.next();
+        System.out.println("Enter your City");
+        String city = newData.next();
+        System.out.println("Enter Your State");
+        String state = newData.next();
+        System.out.println("Enter Your ZipCode");
+        int zipCode = newData.nextInt();
+        System.out.println("Enter Your MobileNo");
+        long mobileNo = newData.nextLong();
+        System.out.println("Enter Your EmailId");
+        String emailId = newData.next();
+        List<PersonDetails> contactList = new ArrayList<>();
+        PersonDetails contact = new PersonDetails(firstName, lastName, area, city, state, zipCode, mobileNo, emailId);
+        ContactDetails contactDetails = new ContactDetails(contactList);
+        contactList.add(contact);
+        contactDisplay(contactDetails);
     }
+
 }
